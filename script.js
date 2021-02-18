babcd99187730795577fea0c6f710a71e1dae818
Alexey <aleksei.b91163@gmail.com>
2021-02-18 09:36:49 +0300

    update

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    const body = fs.readFileSync(`./public${req.url}`);
-    res.end(body);
+    try {
+        const body = fs.readFileSync(`./public${req.url}`);
+        res.end(body);
+    } catch (err) {
+        const body = fs.readFileSync(`./public/index.html`);
+        res.end(body);
+    }
 });
 
 const port = process.env.PORT || 3000;
