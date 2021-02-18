18eb339c15e2161fce0ad55f1665fb753fd91ad8
Alexey <aleksei.b91163@gmail.com>
2021-02-18 10:02:49 +0300

    update

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     try {
+        if (req.url == '/favicon.ico') {
+            res.setHeader('Content-Type', 'image');
+            fs.createReadStream('./public/favicon.ico').pipe(res);
+        }
         const body = fs.readFileSync(`./public${req.url}`);
         res.end(body);
     } catch (err) {
